a2ede7e5d2e761cefa269efa10b1107ff4ea52f2
rahman-a <ahm.abdrahman@gmail.com>
2021-09-16 16:39:05 +0200

    change the server port

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 import express from 'express'
 import cors from 'cors'
-import helmet from 'helmet'
+import dotenv from 'dotenv'
 import {fileURLToPath} from 'url'
 import path from 'path' 
 
@@ -8,14 +8,17 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
 
 const app = express()
-
+dotenv.config()
 app.use(cors())
-// app.use(helmet())
+
 
 app.use(express.static(path.resolve(__dirname, 'client/build')))
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'client/build/index.html'))
 })
 
-const port  = 5000 || process.env.PORT
-app.listen(port)
+const port  =  process.env.PORT || 5000
+
+app.listen(port, () => {
+    console.log(`Server is running at ${port}`);
+})
